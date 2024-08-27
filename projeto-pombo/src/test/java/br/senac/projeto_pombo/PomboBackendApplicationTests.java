909e90f01eb86fd6c470b742d263a2184696d7b4
Tati Sakuma <tati.sakuma@gmail.com>
2024-08-27 10:18:01 -0300

    iniciando

---
+++ projeto-pombo/src/test/java/br/senac/projeto_pombo/PomboBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.projeto_pombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomboBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
