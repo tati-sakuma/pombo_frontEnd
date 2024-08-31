a04585855f6b060b4eeea6a84148bad035f348eb
Tatiana Sakuma <tati.sakuma@gmail.com>
2024-08-31 08:51:20 -0300

    Ajustando entidades

--- projeto-pombo/src/main/java/br/senac/projeto_pombo/model/entity/Pruu.java
+++ projeto-pombo/src/main/java/br/senac/projeto_pombo/model/entity/Pruu.java
@@ -1,5 +1,40 @@
 package br.senac.projeto_pombo.model.entity;
 
-public class Pruu {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table
+public class Pruu {
+	@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+	@NotBlank(message = "Use sua criatividade! Digite algo!")
+	@Size(min = 1, max = 300)
+	private String mensagem;
+	@CreationTimestamp
+	private LocalDateTime dataHora;
+	@ColumnDefault("0")
+	private Integer curtidas;
+	@ColumnDefault("0")
+	private Integer deuncias;
+	@ManyToOne
+    @JoinColumn(name="id_usuario", nullable=false)
+	private Usuario usuario;
+	 
 }
--- projeto-pombo/src/main/java/br/senac/projeto_pombo/model/entity/Usuario.java
+++ projeto-pombo/src/main/java/br/senac/projeto_pombo/model/entity/Usuario.java
@@ -1,10 +1,15 @@
 package br.senac.projeto_pombo.model.entity;
 
+import java.util.Set;
+
+import org.hibernate.annotations.ColumnDefault;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -14,11 +19,17 @@ import lombok.Data;
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private int idUsuario;
 	@Column(nullable = false)
 	private String nome;
 	@Column(nullable = false)
 	private String email;
 	@Column(nullable = false)
+	//@CPF
 	private String cpf;
+	@ColumnDefault("false")
+	private Boolean administrador;
+	@OneToMany(mappedBy="usuario")
+	private Set<Pruu> pruus;
+ 
 }
