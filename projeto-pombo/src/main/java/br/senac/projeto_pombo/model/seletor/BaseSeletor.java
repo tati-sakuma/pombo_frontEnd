46cc25e0a9efbba1e91d4afbd03e488c6066ba47
Tatiana Sakuma <tati.sakuma@gmail.com>
2024-09-28 09:01:18 -0300

    Seletor de usuário.

--- projeto-pombo/src/main/java/br/senac/projeto_pombo/model/seletor/BaseSeletor.java
+++ projeto-pombo/src/main/java/br/senac/projeto_pombo/model/seletor/BaseSeletor.java
@@ -4,5 +4,20 @@ import lombok.Data;
 
 @Data
 public abstract class BaseSeletor {
-	
+	private int pagina;
+	private int limite;
+
+	public BaseSeletor() {
+		this.limite = 0;
+		this.pagina = 0;
+	}
+
+	public boolean temPaginacao() {
+		return this.limite > 0 && this.pagina > 0;
+	}
+
+	public boolean stringValida(String texto) {
+		return texto != null && !texto.isBlank();
+	}
+
 }
--- projeto-pombo/src/main/java/br/senac/projeto_pombo/model/seletor/UsuarioSeletor.java
+++ projeto-pombo/src/main/java/br/senac/projeto_pombo/model/seletor/UsuarioSeletor.java
@@ -1,7 +1,11 @@
 package br.senac.projeto_pombo.model.seletor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.jpa.domain.Specification;
 
+import br.senac.projeto_pombo.exception.PomboException;
 import br.senac.projeto_pombo.model.entity.Usuario;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -10,18 +14,37 @@ import jakarta.persistence.criteria.Root;
 import lombok.Data;
 
 @Data
-public class UsuarioSeletor implements Specification<Usuario> {
+public class UsuarioSeletor extends BaseSeletor implements Specification<Usuario> {
 
 	private String nome;
 	private String email;
 	private String cpf;
-	private Boolean administrador;
+	
+	public boolean temFiltro() {
+		return (this.stringValida(this.nome))
+				|| (this.stringValida(this.email))
+				|| (this.stringValida(this.cpf))
+				;
+	} 
 	
 	@Override
-	public Predicate toPredicate(Root<Usuario> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-	
+	public Predicate toPredicate(Root<Usuario> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		List<Predicate> condicoes = new ArrayList<>();
 		
-		return null;
+		//condição para filtrar nome, verificação se está branco ou nulo feito pelo temFiltro
+		if(this.getNome().trim().length() > 0) {
+			condicoes.add(cb.like(root.get("nome"), "%" + this.getNome() + "%"));
+		}
+		
+		if(this.getEmail().trim().length() > 0) {
+			condicoes.add(cb.like(root.get("email"), "%" + this.getEmail() + "%"));
+		}
+		
+		if(this.getCpf().trim().length() > 0) {
+			condicoes.add(cb.like(root.get("cpf"), "%" + this.getCpf() + "%"));
+		}
+		
+		return cb.and(condicoes.toArray(new Predicate[0]));
 	}
 
 }
