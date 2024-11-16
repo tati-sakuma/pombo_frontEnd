915ef6602e40cec6cf5b45b6c13fc0b7ff9896bd
Tatiana Sakuma <tati.sakuma@gmail.com>
2024-11-16 08:02:15 -0300

    Ajustando front

---
+++ frontEnd-pombo/frontPombo/src/app/shared/service/usuario.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+
+  private readonly API: 'http://localhost:4200/usuario/api/restrito/usuario';
+
+  constructor(
+    private httpClient: HttpClient,
+
+  ) { }
+
+  public pesquisarTodos(): Observable<Array<Usuario>> {
+    return this.httpClient.get<Array<Usuario>>(this.API);
+  }
+}
--- frontEnd-pombo/frontPombo/src/app/usuario/usuario-listagem/usuario-listagem.component.ts
+++ frontEnd-pombo/frontPombo/src/app/usuario/usuario-listagem/usuario-listagem.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../shared/model/usuario';
+import { UsuarioService } from '../../shared/service/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuario-listagem',
   templateUrl: './usuario-listagem.component.html',
   styleUrl: './usuario-listagem.component.sass'
 })
-export class UsuarioListagemComponent {
+export class UsuarioListagemComponent implements OnInit {
 
+  public usuarios: Usuario[] = [];
+
+  constructor(private usuarioService: UsuarioService){}
+
+  ngOnInit(): void {
+
+
+  }
+
+  private pesquisarTodosUsuarios () {
+    this.usuarioService.pesquisarTodos().subscribe(
+      resultado => {
+        this.usuarios = resultado;
+      },
+      erro => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Erro ao listar todos usuários.',
+          text: 'Erro ao listar todos usuários.' + erro.error.mensagem,
+        });
+      }
+    )
+  }
 }
