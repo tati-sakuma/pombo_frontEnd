9b120d75eac19e7dab6dd7c10e2bbd3a4d370fbe
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-25 08:35:11 -0300

    denuncia-listagem component

---
+++ frontEnd-pombo/frontPombo/src/app/denuncia/denuncia-listagem/denuncia-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-denuncia-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './denuncia-listagem.component.html',
+  styleUrl: './denuncia-listagem.component.sass'
+})
+export class DenunciaListagemComponent {
+
+}
