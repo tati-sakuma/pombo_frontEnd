67d38baf8cdd4025e36251507f2f06010e0b30c7
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-25 08:40:24 -0300

    component usuario/admin-inicio

---
+++ frontEnd-pombo/frontPombo/src/app/usuario/admin-inicio/admin-inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-inicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-inicio.component.html',
+  styleUrl: './admin-inicio.component.sass'
+})
+export class AdminInicioComponent {
+
+}
