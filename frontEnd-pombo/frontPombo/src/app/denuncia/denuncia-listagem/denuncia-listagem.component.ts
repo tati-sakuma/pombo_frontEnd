caa9caee2158f62736635d231a54b14499c76527
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-25 08:56:46 -0300

    Update denunciaListagem.ts e adminInicio.ts

--- frontEnd-pombo/frontPombo/src/app/denuncia/denuncia-listagem/denuncia-listagem.component.ts
+++ frontEnd-pombo/frontPombo/src/app/denuncia/denuncia-listagem/denuncia-listagem.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-denuncia-listagem',
-  standalone: true,
-  imports: [],
   templateUrl: './denuncia-listagem.component.html',
   styleUrl: './denuncia-listagem.component.sass'
 })
--- frontEnd-pombo/frontPombo/src/app/usuario/admin-inicio/admin-inicio.component.ts
+++ frontEnd-pombo/frontPombo/src/app/usuario/admin-inicio/admin-inicio.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-inicio',
-  standalone: true,
-  imports: [],
   templateUrl: './admin-inicio.component.html',
   styleUrl: './admin-inicio.component.sass'
 })
